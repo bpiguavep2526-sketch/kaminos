537c3d8b13754736585e2a08d387c87293a58b27
bpiguavep2526-sketch <bpiguavep2526@politecnics.barcelona>
2026-02-02 19:12:07 +0100

    rutas ejemplos

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <h1>About Page</h1>
+  <p>This is the about page of the application.</p>
+  <RouterLink to="/">Go to Home</RouterLink>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <h1>esto es home</h1>
+  
+</template>
+
+<style scoped></style>
